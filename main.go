d06aa0ff13b4afde5ff8e62bc2f6d30466e0e218
Joe Kim <joesm.genie@gmail.com>
2025-03-11 13:28:57 +0700

    add openapi docs path

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
+	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/coregate/tickets-app/configs"
 	"github.com/coregate/tickets-app/routes"
 	"github.com/gin-gonic/gin"
@@ -13,6 +15,20 @@ func main() {
 	server := gin.New()
 
 	routes.RegisterRoutes(server)
+	server.GET("/api/docs", func(c *gin.Context) {
+		htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
+			SpecURL: "./docs/swagger.json",
+			CustomOptions: scalar.CustomOptions{
+				PageTitle: "Tickets API Reference",
+			},
+			DarkMode: true,
+		})
+
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+		fmt.Fprintln(c.Writer, htmlContent)
+	})
 
 	server.Run(":" + strconv.Itoa(int(configs.App.Port)))
 }
