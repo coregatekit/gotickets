cabebe71a73feae56f8b210f2dc58a2b45ea55cb
Joe Kim <joesm.genie@gmail.com>
2025-03-11 12:30:56 +0700

    add some basic configs

---
+++ configs/configs.go
@@ -0,0 +1,47 @@
+package configs
+
+import "github.com/spf13/viper"
+
+type (
+	Configs struct {
+		App      *App
+		Database *Database
+	}
+
+	App struct {
+		Name string
+		Env  string
+		Port int32
+	}
+
+	Database struct {
+		Host     string
+		Port     int32
+		User     string
+		Password string
+		Name     string
+	}
+)
+
+func NewConfigs() (*Configs, error) {
+	viper.AutomaticEnv()
+
+	app := &App{
+		Name: viper.GetString("APP_NAME"),
+		Env:  viper.GetString("APP_ENV"),
+		Port: viper.GetInt32("APP_PORT"),
+	}
+
+	db := &Database{
+		Host:     viper.GetString("DB_HOST"),
+		Port:     viper.GetInt32("DB_PORT"),
+		User:     viper.GetString("DB_USER"),
+		Password: viper.GetString("DB_PASSWORD"),
+		Name:     viper.GetString("DB_NAME"),
+	}
+
+	return &Configs{
+		App:      app,
+		Database: db,
+	}, nil
+}
