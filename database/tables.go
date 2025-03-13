0d048099836d9c3b2c228d52ede472f801ee5334
Joe Kim <joesm.genie@gmail.com>
2025-03-13 11:30:52 +0700

    fix user model for connect with db

---
+++ database/tables.go
@@ -0,0 +1,11 @@
+package database
+
+const (
+	SchemaAuth    = "auth"
+	SchemaTickets = "tickets"
+)
+
+const (
+	TableUsers   = SchemaAuth + "users"
+	TableTickets = SchemaTickets + "tickets"
+)
--- packages/users/models.go
+++ packages/users/models.go
@@ -1,13 +1,26 @@
 package users
 
 import (
-	"github.com/coregate/tickets-app/common"
+	"github.com/coregate/tickets-app/database"
+	"github.com/nrednav/cuid2"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	common.BaseModel
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        string `json:"id" gorm:"primaryKey"`
+	Name      string `json:"name"`
+	Username  string `json:"username" gorm:"unique"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt string `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+func (u *User) TableName() string {
+	return database.TableUsers
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = cuid2.Generate()
+	return nil
 }
