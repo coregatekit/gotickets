84d19d10187134ad2e1b178f909af6b83f790410
Joe Kim <joesm.genie@gmail.com>
2025-03-14 16:50:19 +0700

    update user model

--- packages/users/models.go
+++ packages/users/models.go
@@ -1,19 +1,21 @@
 package users
 
 import (
+	"time"
+
 	"github.com/coregate/tickets-app/database"
 	"github.com/nrednav/cuid2"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        string `json:"id" gorm:"primaryKey"`
-	Name      string `json:"name"`
-	Username  string `json:"username" gorm:"unique"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"password"`
-	CreatedAt string `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt string `json:"updated_at" gorm:"autoUpdateTime"`
+	ID        string    `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name"`
+	Username  string    `json:"username" gorm:"unique"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (u *User) TableName() string {
