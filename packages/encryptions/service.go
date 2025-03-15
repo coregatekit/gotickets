a59a2aff63e0598de266e36524c646b2f60052bc
Joe Kim <joesm.genie@gmail.com>
2025-03-16 01:04:29 +0700

    add method decode hash

--- packages/encryptions/service.go
+++ packages/encryptions/service.go
@@ -3,7 +3,9 @@ package encryptions
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/coregate/tickets-app/configs"
 	"golang.org/x/crypto/argon2"
@@ -17,6 +19,12 @@ type IEncryptionsService interface {
 	HashPassword(password string) (string, error)
 	ComparePassword(hashedPassword, password string) (bool, error)
 }
+
+var (
+	ErrorInvalidHash               = errors.New("the encoded hash is not in the correct format")
+	ErrorIncompatibleVersion       = errors.New("incompatible version of argon2")
+	ErrorMismatchedHashAndPassword = errors.New("the hash and password do not match")
+)
 
 func NewEncryptionsService(configs *configs.Configs) IEncryptionsService {
 	return &encryptionsService{
@@ -53,4 +61,37 @@ func (s *encryptionsService) generateRandomBytes(n uint32) ([]byte, error) {
 	}
 
 	return b, nil
+}
+
+func (s *encryptionsService) decodeHash(encodedHash string) (*configs.ArgonParams, []byte, []byte, error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, ErrorInvalidHash
+	}
+
+	var version int
+	_, err := fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	p := &configs.ArgonParams{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.Memory, &p.Iterations, &p.Parallelism)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	sal, err := base64.RawStdEncoding.Strict().DecodeString(vals[4])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.SaltLength = uint32(len(sal))
+
+	hash, err := base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.KeyLength = uint32(len(hash))
+
+	return p, sal, hash, nil
 }
