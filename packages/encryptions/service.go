347bc3e45374cbce13e373b9f2b0910a781390a6
Joe Kim <joesm.genie@gmail.com>
2025-03-16 00:11:55 +0700

    implement method hash password

--- packages/encryptions/service.go
+++ packages/encryptions/service.go
@@ -1,15 +1,45 @@
 package encryptions
 
+import (
+	"crypto/rand"
+
+	"golang.org/x/crypto/argon2"
+)
+
 type encryptionsService struct{}
 
 type IEncryptionsService interface {
-	HashPassword(password string) (string, error)
+	HashPassword(password string) ([]byte, error)
 }
 
 func NewEncryptionsService() IEncryptionsService {
 	return &encryptionsService{}
 }
 
-func (s *encryptionsService) HashPassword(password string) (string, error) {
-	return "", nil
+func (s *encryptionsService) HashPassword(password string) ([]byte, error) {
+	p := Params{
+		Memory:      64 * 1024,
+		Iterations:  3,
+		Parallelism: 2,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+
+	salt, err := s.generateRandomBytes(p.SaltLength)
+	if err != nil {
+		return nil, err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
+	return hash, nil
+}
+
+func (s *encryptionsService) generateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
 }
