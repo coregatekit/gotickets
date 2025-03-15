38e00a2ca30ba5995b5ef7e628516f523ed5d738
Joe Kim <joesm.genie@gmail.com>
2025-03-16 00:03:56 +0700

    implement some basic encryptions package

--- packages/encryptions/service.go
+++ packages/encryptions/service.go
@@ -1,1 +1,15 @@
 package encryptions
+
+type encryptionsService struct{}
+
+type IEncryptionsService interface {
+	HashPassword(password string) (string, error)
+}
+
+func NewEncryptionsService() IEncryptionsService {
+	return &encryptionsService{}
+}
+
+func (s *encryptionsService) HashPassword(password string) (string, error) {
+	return "", nil
+}
