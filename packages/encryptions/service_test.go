f41c9785f776059eb06d0078c441984070941ca6
Joe Kim <joesm.genie@gmail.com>
2025-03-16 00:20:20 +0700

    add test case for handle error when an error occured on hash password

--- packages/encryptions/service_test.go
+++ packages/encryptions/service_test.go
@@ -1,11 +1,19 @@
 package encryptions_test
 
 import (
+	"crypto/rand"
+	"errors"
 	"testing"
 
 	"github.com/coregate/tickets-app/packages/encryptions"
 	"github.com/stretchr/testify/assert"
 )
+
+type errorReader struct{}
+
+func (e *errorReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("mocked error")
+}
 
 func TestHashPassword(t *testing.T) {
 	t.Run("should hash a password successfully", func(t *testing.T) {
@@ -19,5 +27,24 @@ func TestHashPassword(t *testing.T) {
 		// Assert
 		assert.NoError(t, err)
 		assert.NotEmpty(t, hashedPassword)
+	})
+
+	t.Run("should return error when cannot generate random bytes", func(t *testing.T) {
+		// Arrange
+		password := "password"
+		crypto := encryptions.NewEncryptionsService()
+
+		originalReader := rand.Reader
+		defer func() {
+			rand.Reader = originalReader
+		}()
+		rand.Reader = &errorReader{}
+
+		// Act
+		hashedPassword, err := crypto.HashPassword(password)
+
+		// Assert
+		assert.Error(t, err)
+		assert.Empty(t, hashedPassword)
 	})
 }
