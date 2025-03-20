c602cfea09e9ee66ee8e9c27d0be3f0005dd1e5f
Joe Kim <joesm.genie@gmail.com>
2025-03-20 17:39:35 +0700

    add login request struct

--- pkg/auth/models.go
+++ pkg/auth/models.go
@@ -6,3 +6,8 @@ type CreateUser struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
