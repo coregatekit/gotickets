dc816b65c5c220911f8fa9204ff90a0b22014f5f
Joe Kim <joesm.genie@gmail.com>
2025-03-21 10:47:18 +0700

    move health check route to method register route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"strconv"
 
-	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/coregate/tickets-app/configs"
 	"github.com/coregate/tickets-app/database"
 	"github.com/coregate/tickets-app/routes"
@@ -22,20 +20,6 @@ func main() {
 	}
 
 	routes.RegisterRoutes(server, dbConnection.DB, configs)
-	server.GET("/api/docs", func(c *gin.Context) {
-		htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
-			SpecURL: "./docs/swagger.json",
-			CustomOptions: scalar.CustomOptions{
-				PageTitle: "Tickets API Reference",
-			},
-			DarkMode: true,
-		})
-
-		if err != nil {
-			fmt.Printf("Error: %v", err)
-		}
-		fmt.Fprintln(c.Writer, htmlContent)
-	})
 
 	err = server.Run(":" + strconv.Itoa(int(configs.App.Port)))
 	if err != nil {
--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,30 @@
 package routes
 
 import (
+	"fmt"
+
+	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/coregate/tickets-app/configs"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func RegisterRoutes(server *gin.Engine, db *gorm.DB, configs *configs.Configs) {
+	server.GET("/api/docs", func(c *gin.Context) {
+		htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
+			SpecURL: "./docs/swagger.json",
+			CustomOptions: scalar.CustomOptions{
+				PageTitle: "Tickets API Reference",
+			},
+			DarkMode: true,
+		})
+
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+		fmt.Fprintln(c.Writer, htmlContent)
+	})
+
 	HealthCheckRoutes(server)
 	AuthRoutes(server, db, configs)
 }
