917b553fe2b43bc9ccbf2654588793e565b0773b
Joe Kim <joesm.genie@gmail.com>
2025-03-21 11:06:53 +0700

    add structure for login result

--- pkg/auth/models.go
+++ pkg/auth/models.go
@@ -11,3 +11,8 @@ type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+type LoginResult struct {
+	Token        string `json:"token"`
+	RefreshToken string `json:"refresh_token"`
+}
